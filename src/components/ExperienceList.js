9cacf8f14bc97ca9b311d04c01e735d9e16a20ee
= <cameron@camerongreens.com>
2019-07-02 14:24:13 -0500

    swapping screens

--- src/components/ExperienceList.js
+++ src/components/ExperienceList.js
@@ -4,6 +4,8 @@ import { StyledList, StyledListItem, StyledListButton } from '../StyledComponent
 
 export function ExperienceList(props) {
 	const apiToken = props.token
+	const setCurrentExperience = props.setCurrentExperience
+
 	const [experiences, setExperiences] = useState([])
 
 	useEffect(() => {
@@ -16,7 +18,7 @@ export function ExperienceList(props) {
 			{experiences &&
 				experiences.map(experience => {
 					return (
-						<StyledListItem key={experience.EngagementId} onClick={() => console.log({ experience })}>
+						<StyledListItem key={experience.EngagementId} onClick={() => getExperience(experience.EngagementId, apiToken)}>
 							{experience.Asset && <img src={`${process.env.REACT_APP_IMAGE_BASEURL}/${experience.Asset}`} alt="" width="100px" />}
 							<StyledListButton> {experience.Name}</StyledListButton>
 						</StyledListItem>
@@ -29,6 +31,12 @@ export function ExperienceList(props) {
 		const indexResponse = await getData(`${process.env.REACT_APP_API_ENDPOINT}/Engagement/Index`, { token })
 		if (indexResponse) {
 			setExperiences(indexResponse)
+		}
+	}
+	async function getExperience(EngagementId, token) {
+		const experienceDetailResponse = await getData(`${process.env.REACT_APP_API_ENDPOINT}/Runtime/${EngagementId}`, { token })
+		if (experienceDetailResponse) {
+			setCurrentExperience(experienceDetailResponse)
 		}
 	}
 }
