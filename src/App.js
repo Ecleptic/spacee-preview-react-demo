282f3c61dcc69d2c987e4ea3f1c00d68fb3f047b
= <cameron@camerongreens.com>
2019-07-02 13:32:54 -0500

    app js setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import { GlobalStyle } from './StyledComponents/GlobalStyle'
+import { Header } from './StyledComponents/Header'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		return () => {}
+	}, [])
+	return (
+		<>
+			<GlobalStyle />
+			<div className="App">{/* App Data Here */}</div>
+		</>
+	)
 }
 
-export default App;
+export default App
